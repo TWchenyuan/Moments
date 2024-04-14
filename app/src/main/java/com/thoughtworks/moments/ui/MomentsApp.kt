677190efae0d012a1af2f3369f02d0366c04df48
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-14 10:36:33 +0800

    add moments app

---
+++ app/src/main/java/com/thoughtworks/moments/ui/MomentsApp.kt
@@ -0,0 +1,9 @@
+package com.thoughtworks.moments.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MomentsApp() {
+    Text("Hello world")
+}
