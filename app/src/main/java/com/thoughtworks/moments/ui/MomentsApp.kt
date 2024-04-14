447da8bbb88056ff510596cd50ac062429066f89
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-14 14:24:17 +0800

    add navigation

--- app/src/main/java/com/thoughtworks/moments/ui/MomentsApp.kt
+++ app/src/main/java/com/thoughtworks/moments/ui/MomentsApp.kt
@@ -1,9 +1,44 @@
 package com.thoughtworks.moments.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MomentsApp() {
   Text("Hello world")
+}
+
+@Composable
+fun MomentsNavigationWrapper() {
+}
+
+@Composable
+fun MomentsAppContent(modifier: Modifier = Modifier) {
+  val navController = rememberNavController()
+  Column(modifier = modifier.fillMaxSize()) {
+    MomentsNavHost(modifier = modifier, navController = navController)
+  }
+}
+
+@Composable
+fun MomentsNavHost(modifier: Modifier, navController: NavHostController) {
+  NavHost(modifier = modifier, navController = navController, startDestination = "") {
+    composable("Chat") {
+    }
+    composable("Contact") {
+    }
+
+    composable("Discover") {
+    }
+
+    composable("Me") {
+    }
+  }
 }
