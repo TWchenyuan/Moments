2e642b4c584968a3bc9a784aa97b309fa49ad104
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-16 15:40:21 +0800

    using api

--- app/src/main/java/com/thoughtworks/moments/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/com/thoughtworks/moments/data/repository/AccountRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.thoughtworks.moments.data.repository
 import com.thoughtworks.moments.data.Account
 import com.thoughtworks.moments.data.network.AccountAPI
 
-class AccountRepositoryImpl(accountApi: AccountAPI) : AccountRepository {
+class AccountRepositoryImpl(val accountApi: AccountAPI) : AccountRepository {
   override suspend fun currentAccount(): Account {
     TODO("Not yet implemented")
   }
--- app/src/main/java/com/thoughtworks/moments/data/repository/MomentRepositoryImpl.kt
+++ app/src/main/java/com/thoughtworks/moments/data/repository/MomentRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.thoughtworks.moments.data.repository
 import com.thoughtworks.moments.data.Moment
 import com.thoughtworks.moments.data.network.MomentAPI
 
-class MomentRepositoryImpl(momentApi: MomentAPI) : MomentRepository {
+class MomentRepositoryImpl(val momentApi: MomentAPI) : MomentRepository {
   override suspend fun latestMoments(): List<Moment> {
     TODO("Not yet implemented")
   }
